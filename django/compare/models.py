4d01590d06476e016b2cfd75fbba2433ee282f5f
Ahrayz <Ahrayz@users.noreply.github.com>
2018-03-21 23:01:29 +0800

    -
    -

--- django/compare/models.py
+++ django/compare/models.py
@@ -1,15 +1,10 @@
 from django.db import models
+from schools.models import Kindergarten
 
 class CompareList(models.Model):
-    name = models.CharField(max_length=80,unique=True)
+    Kindergarten = Kindergarten
+    rating = models.IntegerField()
     #compared = models.ForeignKey()#TODO
 
     def __str__(self):
         return self.name
-
-class Result(models.Model):
-    size = models.IntegerField()
-    winner = models.CharField(max_length=80,unique=True)
-    type = models.IntegerField()
-    def __str__(self):
-        return self.winner
--- django/compare/views.py
+++ django/compare/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import Compare
+from .models import CompareList,Result
 
 def index(request):
     my_dict={'dic_compare':'This is from compare.'}
     #return HttpResponse("<em>compare</em>")
+    return render(request,'index.html',context=my_dict)
+
+def result(request):
+    compareList=CompareList.objects.order_by('data')
+    my_dict={'dic_compare':'This is from compare.'}
     return render(request,'index.html',context=my_dict)
 
 # def school_info(request, centre_code):
